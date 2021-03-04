9444c2252268a759e8adb2b224b04d7a81351b76
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-03-04 10:00:26 +0300

    Add app context for ef with sql server

---
+++ bookstore-restapi/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using bookstore_restapi.Models;
+
+namespace bookstore_restapi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable(nameof(Book));
+        }
+    }
+}
--- bookstore-restapi/Startup.cs
+++ bookstore-restapi/Startup.cs
@@ -11,6 +11,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using bookstore_restapi.Models;
+using bookstore_restapi.Data;
 
 namespace bookstore_restapi
 {
@@ -26,7 +29,8 @@ namespace bookstore_restapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
