b5cb494dec18d9efbb6c22fdc9bddacb46045e1d
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-03-04 13:55:36 +0300

    Add Price to book. Add initialCreate migration

--- bookstore-restapi/Models/Book.cs
+++ bookstore-restapi/Models/Book.cs
@@ -10,5 +10,6 @@ namespace bookstore_restapi.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
+        public double Price { get; set; }
     }
 }
