42d151c6edef251a535d5dcb8c36a115275fbec2
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-03-24 22:13:03 +0300

    Add dto fix to book controller api

--- bookstore-restapi/Controllers/BooksController.cs
+++ bookstore-restapi/Controllers/BooksController.cs
@@ -24,14 +24,16 @@ namespace bookstore_restapi.Controllers
 
         // GET: api/Books
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        public async Task<ActionResult<IEnumerable<BookDTO>>> GetBooks()
         {
-            return await _context.Books.ToListAsync();
+            return await _context.Books.Select(b =>
+                new BookDTO { Id = b.Id, Author = b.Author,
+                    Name = b.Name, Price = b.Price }).ToListAsync();
         }
 
         // GET: api/Books/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(long id)
+        public async Task<ActionResult<BookDTO>> GetBook(long id)
         {
             var book = await _context.Books.FindAsync(id);
 
@@ -40,19 +42,22 @@ namespace bookstore_restapi.Controllers
                 return NotFound();
             }
 
-            return book;
+            return new BookDTO { Id = book.Id, Author = book.Author,
+                Name = book.Name, Price = book.Price };
         }
 
         // PUT: api/Books/5
         [HttpPut("{id}")]
         [Authorize("change:books")]
-        public async Task<IActionResult> PutBook(long id, Book book)
+        public async Task<IActionResult> PutBook(long id, BookDTO bookDTO)
         {
-            if (id != book.Id)
+            if (id != bookDTO.Id)
             {
                 return BadRequest();
             }
 
+            var book = new Book { Id = bookDTO.Id, Author = bookDTO.Author,
+                Name = bookDTO.Name, Price = bookDTO.Price };
             _context.Entry(book).State = EntityState.Modified;
 
             try
@@ -77,12 +82,24 @@ namespace bookstore_restapi.Controllers
         // POST: api/Books
         [HttpPost]
         [Authorize("change:books")]
-        public async Task<ActionResult<Book>> PostBook(Book book)
+        public async Task<ActionResult<BookDTO>> PostBook(BookForPostDTO bookForPostDTO)
         {
+            var book = new Book
+            {
+                Author = bookForPostDTO.Author,
+                Name = bookForPostDTO.Name,
+                Price = bookForPostDTO.Price
+            };
             _context.Books.Add(book);
             await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+            var bookDTO = new BookDTO
+            {
+                Id = book.Id,
+                Author = bookForPostDTO.Author,
+                Name = bookForPostDTO.Name,
+                Price = bookForPostDTO.Price
+            };
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, bookDTO);
         }
 
         // DELETE: api/Books/5
---
+++ bookstore-restapi/Models/BookForPostDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookstore_restapi.Models
+{
+    public class BookForPostDTO
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+}
